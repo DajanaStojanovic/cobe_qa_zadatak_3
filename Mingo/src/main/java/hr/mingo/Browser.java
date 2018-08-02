1590631b88dfc1800a29b9bc010c084117129c31
Dajana Stojanović <daja15@live.com>
2018-08-02 20:05:13 +0200

    Update Browser.java

--- Mingo/src/main/java/hr/mingo/Browser.java
+++ Mingo/src/main/java/hr/mingo/Browser.java
@@ -1,27 +1,19 @@
 package hr.mingo;
 
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
 import hr.mingo.variables.GlobalVariables;
 
 public class Browser extends GlobalVariables{
-	
 	static WebDriver driver = new ChromeDriver();
-	
-	
-	
+
 	public static void goTo(String url) {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Printer\\Desktop\\Projekti\\JavaTesting\\drivers\\chromedriver.exe");
-		
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--start-maximized");
-		
 		driver = new ChromeDriver(options);
-		driver.navigate().to(url);
-		
+		driver.navigate().to(url);	
 	}
 
 	public static String title() {
